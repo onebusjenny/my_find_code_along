b0eb7c42fb6208b91fd8539cb71659d544882725
Jenny Chu <jchudesign0930@gmail.com>
2019-10-09 21:45:28 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,26 @@
 require 'pry'
 
-def my_find(collection)
+collection = (1..100).to_a
 
+def my_find(collection)
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i += 1
+  end
+end
+
+puts my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
+puts 
+puts 
+puts "Here is the Second set"
+
+def my_find2(collection)
+  nu_array = []
+  i = 0
+  while i < collection.length
+    nu_array.push(collection[i]) if yield(collection[i])
+    i = i + 1
+  end
+  nu_array
 end
